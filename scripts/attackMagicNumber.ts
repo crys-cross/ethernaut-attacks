4ce802d63012efa7b35ff6e5e0cce750f534c897
Crys <mariano.christopher123@gmail.com>
2022-11-07 21:26:19 +0800

    updated attack for magic number level

--- scripts/attackMagicNumber.ts
+++ scripts/attackMagicNumber.ts
@@ -4,16 +4,26 @@ const attackMagicNumber = async () => {
     // change contract addresses here. change gas only if needed.
     const magicNumberAddress = "0xb19100EB1FfB398Aba2b7774e4b345C25e3e0061";
     const playerAddress = "0x3C4f1C7Ab126a94016CA8F4e770522810aa61954";
-    const bytecode = "0x6F40A0F4fD7B7dd225b85F7e8A73a9cb9e024321";
+    const bytecode = "600a600c600039600a6000f3602a60505260206050f3";
 
     // const GOERLI_RPC_URL = "/*Place infura or alchemy link here*/";
     // don't touch below
     const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
     const provider = new ethers.providers.JsonRpcProvider(GOERLI_RPC_URL);
+    const signer = await provider.getSigner();
+    const tx1 = await signer.sendTransaction({
+        from: playerAddress,
+        to: magicNumberAddress,
+        data: bytecode,
+    });
+    const txReceipt1 = await tx1.wait(1);
+    console.log(txReceipt1);
+    const _solver = tx1.to;
     const ABI = ["function setSolver(address _solver)"];
     const contract = await ethers.getContractAt(ABI, magicNumberAddress);
-    const tx = await contract.setSolver(playerAddress);
-    const txReceipt = await tx.wait(1);
+    const tx2 = await contract.setSolver(playerAddress);
+    const txReceipt = await tx2.wait(1);
+    console.log(txReceipt);
 };
 
 attackMagicNumber()

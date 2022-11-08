215c0e4cb83e96e75783014902304b9df37fea70
Crys <mariano.christopher123@gmail.com>
2022-11-08 22:11:32 +0800

    updated alien codex attack

--- scripts/attackAlienCodex.ts
+++ scripts/attackAlienCodex.ts
@@ -7,7 +7,12 @@ const attackAlienCodex = async () => {
     const alienCodexAddress = "0xF675Be946818f77Ba00aD069C99C97080800656E";
     const playerAddress = "0x3C4f1C7Ab126a94016CA8F4e770522810aa61954";
 
-    // notes here
+    // Don't touch below
+    // Exploit comes from no check for underflow on retract function and changing storage value for owner
+    // You may read more on:
+    // https://programtheblockchain.com/posts/2018/03/09/understanding-ethereum-smart-contract-storage/
+    // https://docs.soliditylang.org/en/v0.6.0/security-considerations.html#two-s-complement-underflows-overflows
+    // https://ethereum.stackexchange.com/questions/63403/in-solidity-how-does-the-slot-assignation-work-for-storage-variables-when-there
     const ABI = [
         "function make_contact()",
         "function retract()",

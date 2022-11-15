daa872fa658b2945bd5f94439c1a69662028dcf9
Crys <mariano.christopher123@gmail.com>
2022-11-15 20:55:29 +0800

    updated attack puzzle wallet level

--- scripts/attackPuzzleWallet.ts
+++ scripts/attackPuzzleWallet.ts
@@ -1,8 +1,28 @@
+import { Wallet } from "ethers";
 import { ethers } from "hardhat";
 
 const attackPuzzleWallet = async () => {
     // change contract addresses here.
     const puzzleWalletAddress = "0xAc7EcD98B60C2094BdD63eEaF73436ebE8A28e9a"; //type "await contract.address()" in ethernaut console
+
+    // The vulnerability here arises due to storage collision between the proxy contract (PuzzleProxy) and logic contract (PuzzleWallet)
+    const provider = await ethers.getDefaultProvider("goerli");
+    /*Private Keys in .env file or hardcode here*/
+    const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+    const wallet = new Wallet(PRIVATE_KEY, provider);
+
+    const tx1 = await wallet.sendTransaction({
+        from: "",
+        data: "",
+    });
+    const txReceipt1 = await tx1.wait(1);
+    console.log(txReceipt1);
+    const ABI = [
+        "function approve(address spender, uint amount)",
+        "function swap(address from, address to, uint amount)",
+        "function balanceOf(address token, address account)",
+    ];
+    const contractAttack = await ethers.getContractAt(ABI, puzzleWalletAddress);
 };
 
 attackPuzzleWallet()

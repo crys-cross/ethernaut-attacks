2ff6d8f81778065f6abfeb624ac05a66be63b579
Crys <mariano.christopher123@gmail.com>
2022-12-10 12:05:54 +0800

    updated hello ethernaut level

--- scripts/attackHelloEthernaut.ts
+++ scripts/attackHelloEthernaut.ts
@@ -7,7 +7,7 @@ const attackDoubleEntry = async () => {
     // Don't touch below 🚀
     // Vulnerability from public view of password
     // typing all commands in console below
-    //type contract.abi in ethernaut to expose all ABI
+    // type contract.abi in ethernaut to expose all ABI (change this only if there was an update in ethernaut and this is no longer the same)
     const ABI = [
         {
             inputs: [{ internalType: "string", name: "_password", type: "string" }],
@@ -19,12 +19,14 @@ const attackDoubleEntry = async () => {
         {
             inputs: [{ internalType: "string", name: "passkey", type: "string" }],
             name: "authenticate",
+            outputs: [],
             stateMutability: "nonpayable",
             type: "function",
         },
         {
             inputs: [],
             name: "getCleared",
+            outputs: [{ internalType: "bool", name: "", type: "bool" }],
             stateMutability: "view",
             type: "function",
         },
@@ -38,43 +40,54 @@ const attackDoubleEntry = async () => {
         {
             inputs: [],
             name: "info1",
-            outputs: Array(1),
+            outputs: [{ internalType: "string", name: "", type: "string" }],
             stateMutability: "pure",
             type: "function",
         },
         {
             inputs: [{ internalType: "string", name: "param", type: "string" }],
             name: "info2",
+            outputs: [{ internalType: "string", name: "", type: "string" }],
             stateMutability: "pure",
             type: "function",
         },
         {
             inputs: [],
             name: "info42",
+            outputs: [{ internalType: "string", name: "", type: "string" }],
             stateMutability: "pure",
             type: "function",
         },
         {
+            constant: true,
             inputs: [],
             name: "infoNum",
+            outputs: [{ internalType: "uint8", name: "", type: "uint8" }],
+            payable: undefined,
+            signature: "0xc253aebe",
             stateMutability: "view",
             type: "function",
         },
         {
             inputs: [],
             name: "method7123949",
+            outputs: [{ internalType: "string", name: "", type: "string" }],
             stateMutability: "pure",
             type: "function",
         },
         {
             inputs: [],
             name: "password",
+            outputs: [{ internalType: "string", name: "", type: "string" }],
+            payable: undefined,
+            signature: "0x224b610b",
             stateMutability: "view",
             type: "function",
         },
         {
             inputs: [],
             name: "theMethodName",
+            outputs: [{ internalType: "string", name: "", type: "string" }],
             stateMutability: "view",
             type: "function",
         },
@@ -88,14 +101,19 @@ const attackDoubleEntry = async () => {
     console.log(tx3);
     const tx4 = (await contract.infoNum()).toString();
     console.log(tx4);
-    const tx5 = await contract.info42();
+    const tx5 = (await contract.info42()).toString();
     console.log(tx5);
-    const tx6 = await contract.theMethodName();
+    const tx6 = (await contract.theMethodName()).toString();
     console.log(tx6);
-    const tx7 = await contract.method7123949();
+    const tx7 = (await contract.method7123949()).toString();
     console.log(tx7);
-
-    // console.log(txReceipt1);
+    const tx8 = (await contract.password()).toString();
+    console.log(tx8);
+    const tx9 = await contract.authenticate(tx8);
+    console.log(tx9);
+    const tx10 = await contract.getCleared();
+    console.log(`Passed this level: ${tx10}`);
+    console.log("You may submit at ethernaut if above state 'true'");
 };
 
 attackDoubleEntry()

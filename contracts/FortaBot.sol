4a18e200f594d3992a34022de607b4fcab980db1
Crys <mariano.christopher123@gmail.com>
2023-01-12 22:23:53 +0800

    updated double entry point level

--- contracts/FortaBot.sol
+++ contracts/FortaBot.sol
@@ -1,4 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity <0.8.0;
 
-contract FortaBot {}
+interface IForta {
+    function raiseAlert(address user) external;
+}
+
+contract FortaBot {
+    address private cryptoVault;
+
+    constructor(address _cryptoVault) public {
+        cryptoVault = _cryptoVault;
+    }
+
+    function handleTransaction(address user, bytes calldata msgData) external {
+        // Extract the address of original message sender
+        // which should start at offset 168 (0xa8) of calldata
+        address origSender;
+        assembly {
+            origSender := calldataload(0xa8)
+        }
+
+        if (origSender == cryptoVault) {
+            IForta(msg.sender).raiseAlert(user);
+        }
+    }
+}
